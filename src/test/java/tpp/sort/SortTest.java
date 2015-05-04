c184e46c1a9f4f710c3bac6a6ea9e2f7d83a7d44
saw111 <saw111@pitt.edu>
2015-05-04 15:03:41 -0400

    Extract swap() and outOfOrder() methods and while ==> for.

--- src/test/java/tpp/sort/SortTest.java
+++ src/test/java/tpp/sort/SortTest.java
@@ -26,15 +26,20 @@ public class SortTest {
 	}
 
 	private List<Integer> sort(List<Integer> list) {
-		int index = 0;
-		while (list.size() > index + 1) { // If to While transformation
-			if (list.get(index) > list.get(index + 1)) {
-				Integer temp = list.get(index);
-				list.set(index, list.get(index + 1));
-				list.set(index + 1, temp);
-			}
-			index++;
-		}
+		for (int index = 0; list.size() > index + 1; index++)
+			if (outOfOrder(list, index))
+				swap(list, index);
+
 		return list;
+	}
+
+	private boolean outOfOrder(List<Integer> list, int index) {
+		return list.get(index) > list.get(index + 1);
+	}
+
+	private void swap(List<Integer> list, int index) {
+		Integer temp = list.get(index);
+		list.set(index, list.get(index + 1));
+		list.set(index + 1, temp);
 	}
 }
