9f9c231004479254e58a46836a0fcfbc340d2d01
saw111 <saw111@pitt.edu>
2015-05-04 14:23:30 -0400

    Applied Null transformation.

--- src/test/java/tpp/sort/SortTest.java
+++ src/test/java/tpp/sort/SortTest.java
@@ -1,9 +1,25 @@
 package tpp.sort;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 
 public class SortTest {
 	@Test
-	public void nothing() throws Exception {
+	public void sortings() throws Exception {
+		assertThat(sort(intList()), is(intList()));
+	}
+
+	private List<Integer> sort(List<Integer> list) {
+		return null;
+	}
+
+	private List<Integer> intList() {
+		return Arrays.asList();
 	}
 }
