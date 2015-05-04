19df6ce2d7cbb92f5d3e14b7a0c8621fc588b7dc
saw111 <saw111@pitt.edu>
2015-05-04 14:39:03 -0400

    Tested sort unsorted, 2-element list.

--- src/test/java/tpp/sort/SortTest.java
+++ src/test/java/tpp/sort/SortTest.java
@@ -8,20 +8,20 @@ import java.util.List;
 
 import org.junit.Test;
 
-
 public class SortTest {
 	private void assertSorted(List<Integer> unsorted, List<Integer> sorted) {
 		assertThat(sort(unsorted), is(sorted));
 	}
-	
+
 	private List<Integer> intList(Integer... ints) {
 		return Arrays.asList(ints);
 	}
-	
+
 	@Test
 	public void sortings() throws Exception {
 		assertSorted(intList(), intList());
 		assertSorted(intList(1), intList(1));
+		assertSorted(intList(2, 1), intList(1, 2));
 	}
 
 	private List<Integer> sort(List<Integer> list) {
